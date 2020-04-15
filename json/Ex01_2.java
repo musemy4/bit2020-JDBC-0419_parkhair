6ab064d1f97ebc13bef51a598402b25516e1b9eb
musemy4 <58456250+musemy4@users.noreply.github.com>
2020-04-15 11:55:10 +0900

    Update Ex01_2.java

--- json/Ex01_2.java
+++ json/Ex01_2.java
@@ -26,30 +26,15 @@ public class Json_read {
 		
 			JSONArray jsonArray = JsonObj.getJSONArray("memberArr");
 			
-			//한명씩 받을 벡터
-			Vector<Vector> member = new Vector<>();
 			for(int i=0;i<jsonArray.length();i++) {
 				JSONObject obj = jsonArray.getJSONObject(i);
-				Vector v = new Vector<>();
-				v.add(obj.get("no"));
-				v.add(obj.get("name"));
-				v.add(obj.get("phone"));
-				v.add(obj.get("email"));
-				member.add(v);
+				System.out.println("no: "+obj.get("no"));
+				System.out.println("name: "+obj.get("name"));
+				System.out.println("phone: "+obj.get("phone"));
+				System.out.println("email: "+obj.get("email"));
+				System.out.println("----------------------------");
 			}
-			
-			if(member!=null) {
-				for(int i=0;i<member.size();i++) {
-					System.out.println("no: "+member.get(i).get(0));
-					System.out.println("name: "+member.get(i).get(1));
-					System.out.println("phone: "+member.get(i).get(2));
-					System.out.println("email: "+member.get(i).get(3));
-					
-					System.out.println("-------------------------");
-				}
-			}
-			
-			
+		
 		}
 		
 	}
