d5e101714dd9faf6e95b55bfb46d77af22ab4420
musemy4 <58456250+musemy4@users.noreply.github.com>
2020-04-15 11:52:15 +0900

    Update Ex01_2.java

--- json/Ex01_2.java
+++ json/Ex01_2.java
@@ -1,39 +1,57 @@
-package java_suwop_d21.test.json;
+package json;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-//ÆÄÀÏ ÀĞ¾î¿À±â
-public class Ex01_2 {
+
+public class Json_read {
 
 	public static void main(String[] args) throws IOException {
-		String filepath = "students.json";
-//		InputStream inputStream = Ex01_2.class.getResourceAsStream(filepath);
-//		if(inputStream == null) {
-//			System.out.println("ÆÄÀÏÀ» ÀĞÀ»¼ö ¾ø½À´Ï´Ù!");
-//			return;
-//		}
+		String filepath="src/member.json";
 		
-//		JSONTokener tokener = new JSONTokener(inputStream);
-	
 		File file = new File(filepath);
+		
 		if(file.exists()) {
+			FileReader reader =new FileReader(file);
 			
-			FileReader fileReader;
+			//ì½ì–´ì˜¨ jsoníŒŒì¼ì„ ì§œë¥¸ë‹¤ í°ë‹¨ìœ„ë¡œ ìš°ì„ 
+			JSONTokener token = new JSONTokener(reader);
+			JSONObject JsonObj = new JSONObject(token);
+		
+			JSONArray jsonArray = JsonObj.getJSONArray("memberArr");
 			
+			//í•œëª…ì”© ë°›ì„ ë²¡í„°
+			Vector<Vector> member = new Vector<>();
+			for(int i=0;i<jsonArray.length();i++) {
+				JSONObject obj = jsonArray.getJSONObject(i);
+				Vector v = new Vector<>();
+				v.add(obj.get("no"));
+				v.add(obj.get("name"));
+				v.add(obj.get("phone"));
+				v.add(obj.get("email"));
+				member.add(v);
+			}
 			
-			fileReader= new FileReader(file);
-			
-			JSONTokener tokener = new JSONTokener(fileReader);//¹«¾ùÀ» ±âÁØÀ¸·Î ÀÚ¸£³ª	
-			JSONObject peopleObj = new JSONObject(tokener);
-			
-			
+			if(member!=null) {
+				for(int i=0;i<member.size();i++) {
+					System.out.println("no: "+member.get(i).get(0));
+					System.out.println("name: "+member.get(i).get(1));
+					System.out.println("phone: "+member.get(i).get(2));
+					System.out.println("email: "+member.get(i).get(3));
+					
+					System.out.println("-------------------------");
+				}
+			}
 			
 			
 		}
-	
+		
 	}
 
 }
