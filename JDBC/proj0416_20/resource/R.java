9c2acfe629296d69b5f61df3c2481992e2b78b49
musemy4 <58456250+musemy4@users.noreply.github.com>
2020-04-16 18:13:24 +0900

    Update R.java

--- JDBC/proj0416_20/resource/R.java
+++ JDBC/proj0416_20/resource/R.java
@@ -1,5 +1,6 @@
 package swingMvc_h2.proj0416_20.resource;
 
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import swingMvc_h2.proj0416_20.view.*;
@@ -9,7 +10,10 @@ public interface R {
 	String[] colNames=new String[] {};
 	DefaultTableModel dm = new DefaultTableModel(colNames,0);
 	
-	
+	JTextField tf_name = new JTextField();
+	JTextField tf_phone = new JTextField();
+	JTextField tf_pay = new JTextField();
+	JTextField tf_point = new JTextField();
 	
 	TitleView titleView =new TitleView();
 	InputView inputView =new InputView();
