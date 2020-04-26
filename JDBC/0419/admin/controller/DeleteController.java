4f7745f451dced6ba9e373dcd6146855a21556e1
musemy4 <58456250+musemy4@users.noreply.github.com>
2020-04-26 17:07:39 +0900

    Update DeleteController.java

--- JDBC/0419/admin/controller/DeleteController.java
+++ JDBC/0419/admin/controller/DeleteController.java
@@ -13,17 +13,20 @@ public class DeleteController implements Controller {
 		String idx=(String)JOptionPane.showInputDialog(null, "삭제할 회원의 회원번호를 입력해주세요");
 		
 		DTO dto = new DTO(idx);
-		
-		
-		JOptionPane.showConfirmDialog(null,"진짜로"+idx+"님을 삭제합니까?","", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-
-		
-		
-		dao.delete(dto);
-		dao.deletememserv(dto);
-		
-		
-
+		int rn=dao.isthere(dto);
+		if(rn>0) {
+			JOptionPane.showConfirmDialog(null,"진짜로"+idx+"님을 삭제합니까?","", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+	
+			
+			
+			dao.delete(dto);
+			dao.deletememserv(dto);
+			
+			mlistController.service();
+			slistController.service();
+		} else {
+			JOptionPane.showMessageDialog(null, "해당 idx의 회원이 없습니다");
+		}
 	}
 
 }
